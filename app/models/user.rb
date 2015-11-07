2b6f0e32a416993ac5e6352ee3d22d60095bb64e
shamkeev <shamkeyev@mail.ru>
2015-11-07 14:53:39 +0600

    Installed devise. Added user to devise. Configured active_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :photos
   has_many :comments
+
+  validates :name, presence: true, length: {maximum: 50}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  ActiveAdmin.routes(self)
+  devise_for :users
   root 'photos#index'
 
   resources :photos
