aaa9e6391ba26e1a25a7ccc5cd194c7bc0dbb7a5
shamkeev <shamkeyev@mail.ru>
2015-11-07 18:41:41 +0600

    Added photos fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ password = '12345678'
 User.create!(name:'Administrator', email:'admin@test.com',
              password: password, password_confirmation: password, admin: true )
 
-
+users = []
 5.times do
   user = User.new
   user.name = Faker::Name.name
@@ -11,4 +11,13 @@ User.create!(name:'Administrator', email:'admin@test.com',
   user.password = password
   user.password_confirmation = password
   user.save
+  users.push user
+end
+
+10.times do
+  photo = Photo.new
+  photo.title = Faker::Address.country
+  photo.user = users.sample
+  photo.image = File.open(File.join(Rails.root, "/app/assets/images/fixtures/#{rand(1..8)}.jpg"))
+  photo.save
 end
