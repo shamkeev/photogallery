b39f27572051ef3787c64fe3c13950c36043e8d2
shamkeev <shamkeyev@mail.ru>
2015-11-07 14:37:29 +0600

    Generated migrations for associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107080046) do
+ActiveRecord::Schema.define(version: 20151107083220) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "rating"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "photo_id"
+    t.integer  "user_id"
   end
+
+  add_index "comments", ["photo_id"], name: "index_comments_on_photo_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "photos", force: :cascade do |t|
     t.string   "title"
@@ -28,7 +33,10 @@ ActiveRecord::Schema.define(version: 20151107080046) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "photos", ["user_id"], name: "index_photos_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
