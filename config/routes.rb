4b505d5d0da66f73f5d685827d6b7844d062bfa2
shamkeev <shamkeyev@mail.ru>
2015-11-07 15:27:40 +0600

    Added a route for users show action

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
   devise_for :users
+
+  get 'users/show/:id' => 'users#show', as: :show_user
+
   root 'photos#index'
 
   resources :photos
   resources :comments
+
+
 end
