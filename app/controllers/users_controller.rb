81723a76360292271d047de9f8a8e1f89afe8120
shamkeev <shamkeyev@mail.ru>
2015-11-07 15:41:21 +0600

    Added show action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    @photo = Photo.new
+    @photos = Photo.where(user_id: @user).order('created_at DESC')
   end
 
 end
